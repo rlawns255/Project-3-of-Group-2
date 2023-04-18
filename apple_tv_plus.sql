2eb1de0324633c6aa01a216bea45fd10f5164d50
Trey Lumley <96922295+treysl@users.noreply.github.com>
2023-04-17 20:11:20 -0500

    Update apple_tv_plus.sql
    for trey's computer

--- apple_tv_plus.sql
+++ apple_tv_plus.sql
@@ -32,3 +32,47 @@ ADD CONSTRAINT titles_fk FOREIGN KEY(id) REFERENCES titles(id)
 
 SELECT * FROM credits;
 SELECT * FROM titles;
+
+----------------------
+For Trey's computer
+----------------------
+
+DROP TABLE "titles";
+DROP TABLE "credits";
+
+CREATE TABLE "titles" (
+	"id" varchar(30) PRIMARY KEY,
+	"title" varchar(100),
+	"type" varchar(5),
+	"description" varchar,
+	"release_year" int,
+	"age_certification" varchar(10),
+	"runtime" int,
+	"genres" varchar(250),
+	"production_countries" varchar(250),
+	"seasons" real,
+	"imdb_id" varchar,
+	"imdb_score" real,
+	"imdb_votes" real,
+	"tmdb_popularity" real,
+	"tmdb_score" real
+);
+
+CREATE TABLE "credits" (
+	"person_id" int,
+	"id" varchar, 
+	"name" varchar,
+	"character" varchar,
+	"role" varchar
+);
+
+
+RENAME id TO title_id
+ALTER TABLE credits
+ADD CONSTRAINT titles_fk FOREIGN KEY(title_id) REFERENCES titles(id)
+
+ALTER TABLE credits
+ADD unique_id serial PRIMARY KEY
+
+SELECT * FROM credits;
+SELECT * FROM titles;
