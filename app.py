db06a86b5a9a97439e3b9983339fadfa380e8ff6
hdkronke <hdkronke@gmail.com>
2023-04-18 20:16:05 -0500

    Removed errors from app.py! Looking clean!

--- app.py
+++ app.py
@@ -1,52 +1,53 @@
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-from collections import OrderedDict
+import configparser
 from flask import Flask, jsonify
 from flask_cors import CORS
-import datetime as dt
-import configparser
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+# from collections import OrderedDict
+# import datetime as dt
+# import numpy as np
+# import sqlalchemy
+# from sqlalchemy import func
 
 # Add in postgres credentials
 parser = configparser.ConfigParser()
 parser.read("credentials.conf")
-host = parser.get("postgres_config" , "host_name")
-db_name = parser.get("postgres_config" , "db_name")
-user = parser.get("postgres_config" , "user")
-password = parser.get("postgres_config" , "password")
-port = parser.get("postgres_config" , "port")
+host = parser.get("postgres_config", "host_name")
+db_name = parser.get("postgres_config", "db_name")
+user = parser.get("postgres_config", "user")
+password = parser.get("postgres_config", "password")
+port = parser.get("postgres_config", "port")
 
-### Create engine 
-        ### Create a f string and pass it to a variable
-        ### create engine 
-
-
-
-conn_string = f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'
+# Create engine & f-string, pass it to a variable
+conn_string =\
+    f'postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}'
 engine = create_engine(conn_string)
 
 # Reflect apple_tv database into a model
 Base = automap_base()
-Base.prepare(autoload_with=engine) 
+Base.prepare(autoload_with=engine)
 
-#Create references to class
+# Create references to class
 titles = Base.classes.titles
 credits = Base.classes.credits
 
 # Flask Setup
 app = Flask(__name__)
 CORS(app)
-#Prevent flask from sorting  dictionary keys alphabetically
+
+# Prevent flask from sorting  dictionary keys alphabetically
 app.config['JSON_SORT_KEYS'] = False
+
+# Design app routes
+
 
 @app.route("/")
 def welcome():
+
     "List of available routes"
-    return(
-        f"Welcome to the Apple TV+ API! The available routes are:<br/>"
+    return (
+        f"{'Welcome to the Apple TV+ API! The available routes are:<br/>'}"
         f"<br/>"
         f"/api/v1.0/actors<br/>"
         f"<br/>"
@@ -56,28 +57,32 @@ def welcome():
         f"<br/>"
         f"/api/v1.0/genres<br/>"
     )
-    
+
 # Create route to actors table
-#################################################
+
+
 @app.route("/api/v1.0/actors")
-#################################################
 def actors():
     session = Session(engine)
 
-    #query the actors table
-    results = session.query(titles.title , titles.release_year , titles.imdb_score , credits.name , credits.character)\
-       .filter(titles.id == credits.title_id).all()
-    
+    # Query the actors table
+    results = session.query(titles.title,
+                            titles.release_year,
+                            titles.imdb_score,
+                            credits.name,
+                            credits.character).filter(
+                                titles.id == credits.title_id).all()
+
     # Close the session
     session.close()
 
     # Create a set of actors
     actors = set([row[3] for row in results])
-    
+
     # Create a list of actors
     actors_list = []
 
-    #loop through the actors and append them to the list
+    # Loop through the actors and append them to the list
     for actor in actors:
         actors = {}
         actors['actor'] = actor
@@ -92,8 +97,8 @@ def actors():
                 characters.append(row[4])
                 imdb_scores.append(row[2])
 
-        #append the actors to the list
-        actors['titles'] = movies_shows 
+        # Append the actors to the list
+        actors['titles'] = movies_shows
         actors['release_year'] = release_year
         actors['characters'] = characters
         actors['imdb_scores'] = imdb_scores
@@ -101,19 +106,25 @@ def actors():
 
     return jsonify(actors_list)
 
-#create a route to titles table
-#################################################
+# Create a route to titles table
+
+
 @app.route("/api/v1.0/titles")
-#################################################
 def movies_shows():
 
     # Create a session
     session = Session(engine)
 
     # Query all the movies and shows
-    results = session.query(titles.title , titles.release_year , titles.imdb_score , credits.name , credits.character ,titles.age_certification , titles.description)\
-        .filter(titles.id == credits.title_id).all()
-        
+    results = session.query(titles.title,
+                            titles.release_year,
+                            titles.imdb_score,
+                            credits.name,
+                            credits.character,
+                            titles.age_certification,
+                            titles.description).filter(
+                                titles.id == credits.title_id).all()
+
     # Close the session
     session.close()
 
@@ -123,7 +134,7 @@ def movies_shows():
     # Create a list of movies and shows
     movies_shows_list = []
 
-    #loop through the movies and shows
+    # Loop through the movies and shows
     for name in movies_shows:
         productions = {}
         productions['title'] = name
@@ -140,7 +151,7 @@ def movies_shows():
                 imdb_score.add(row[2])
                 description.add(row[6])
 
-        #append the movies and shows to the list
+        # Append the movies and shows to the list
         productions['release_year'] = list(release_year)[0]
         productions['rating'] = list(rating)[0]
         productions['imdb_score'] = list(imdb_score)[0]
@@ -150,11 +161,10 @@ def movies_shows():
 
     return jsonify(movies_shows_list)
 
-##############################################################################
-
 # Create Age Certification route
-@app.route("/api/v1.0/age-certification")
 
+
+@app.route("/api/v1.0/age-certification")
 def age_cert():
     session = Session(engine)
 
@@ -192,26 +202,29 @@ def age_cert():
 
     return jsonify(age_cert_list)
 
-##############################################################################
+# Create a route to genres table
 
-#Create a route to genres table
-#################################################
+
 @app.route("/api/v1.0/genres")
-#################################################
 def genres():
     session = Session(engine)
-    
+
     # Query all the genres
-    results = session.query(titles.title , titles.genres , titles.release_year , titles.imdb_score , credits.name , credits.character)\
-        .filter(titles.id == credits.title_id).all()
-        
+    results = session.query(titles.title,
+                            titles.genres,
+                            titles.release_year,
+                            titles.imdb_score,
+                            credits.name,
+                            credits.character).filter(
+                                titles.id == credits.title_id).all()
+
     session.close()
-    
+
     genres = set([row[1] for row in results])
-    
+
     genres_list = []
-    
-    #loop through the genres and append them to the list  
+
+    # Loop through the genres and append them to the list
     for genre in genres:
         genres = {}
         genres['genre'] = genre
@@ -225,20 +238,17 @@ def genres():
                 actors.append(row[4])
                 release_year.append(row[2])
                 imdb_scores.append(row[3])
-                
+
         genres['titles'] = movies_shows
         genres['actors'] = actors
         genres['release_year'] = release_year
         genres['imdb_scores'] = imdb_scores
         genres_list.append(genres)
-        
+
     return jsonify(genres_list)
-#################################################
+
+# Run the app
 
 
-
-#################################################
-################### Run the app #################    
-#################################################
 if __name__ == '__main__':
     app.run(debug=True)
